b70daf2507472012618433a7e89e37ef916bb2c5
pengjun0429 <pengjun0429@gmail.com>
2026-07-17 15:32:20 +0800

    Fix OAuth2: remove prompt=none causing auth failure

--- admin/server.js
+++ admin/server.js
@@ -35,7 +35,7 @@ function startAdmin(client) {
   app.get('/login', (req, res) => {
     if (req.session.authenticated) return res.redirect('/dashboard');
     if (useDiscordAuth) {
-      const url = `${DISCORD_API}/oauth2/authorize?client_id=${config.discord.clientId}&redirect_uri=${encodeURIComponent(config.discord.redirectUri)}&response_type=code&scope=identify+guilds&prompt=none`;
+      const url = `${DISCORD_API}/oauth2/authorize?client_id=${config.discord.clientId}&redirect_uri=${encodeURIComponent(config.discord.redirectUri)}&response_type=code&scope=identify+guilds`;
       return res.render('login', { error: null, discordUrl: url, useDiscord: true });
     }
     res.render('login', { error: null, discordUrl: '#', useDiscord: false });
